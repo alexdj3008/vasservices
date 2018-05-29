676e759c4f9d463431ce76a29f1ae38726254dc2
Alexis Goyo <alexisdjesusgoyoflores@gmail.com>
2018-05-29 02:14:18 -0400

    Rol y permiso de administrador implementado

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,10 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if($exception instanceof \Spatie\Permission\Exceptions\UnauthorizedException)
+        {
+            return response()->view('errors.403');
+        }
         return parent::render($request, $exception);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::group(['prefix' => 'paciente',
 //fin de rutas de paciente logueado
 Route::group(['prefix' => 'admin',
     'namespace' => 'Admin',
-    'middleware' => 'auth'],
+    'middleware' => ['role:Admin']],
     function () {
         //Pagina principal de administrador
         Route::get('/', 'AdminController@index')->name('dashboard');
