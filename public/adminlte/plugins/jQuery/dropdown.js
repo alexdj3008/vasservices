555b0a5a79713655d157efcc4858bf206622ce65
Alexis Goyo <alexisdjesusgoyoflores@gmail.com>
2018-06-26 21:41:46 -0400

    prueba 2

--- public/adminlte/plugins/jQuery/dropdown.js
+++ public/adminlte/plugins/jQuery/dropdown.js
@@ -1,18 +1,13 @@
-$("#tratamientos").change(function(event){
-    $.get("cirujanos/"+event.target.value+"",function(response,cirujano){
-        console.log(response);
-    });
-});
 
 $("#especialidades").change(function(event){
     $.get("tratamientos/"+event.target.value+"",function(response,cirujano){
-        console.log(response);
+        $("#tratamientos").empty();
         for(i=0;i<response.length;i++){
             $("#tratamientos").append("<option value='"+response[i].id+"'>"+response[i].nombre+"</option>");
         }
     });
     $.get("cirujanos/"+event.target.value+"",function(response,cirujano){
-        console.log(response);
+        $("#cirujanos").empty();
         for(i=0;i<response.length;i++){
             $("#cirujanos").append("<option value='"+response[i].id+"'>"+response[i].name+"</option>");
         }
